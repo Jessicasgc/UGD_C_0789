de0d6385e7ce0a8a3953ff94b7a744368f9c2cdf
Jessicasgc <jessicaamadearahma@gmail.com>
2022-10-09 12:52:10 +0700

    Cuma ganti TYPO judul page edit Pegawai

--- resources/views/pegawai/edit.blade.php
+++ resources/views/pegawai/edit.blade.php
@@ -4,7 +4,7 @@
     <div class="container-fluid">
         <div class="row mb-2">
             <div class="col-sm-6">
-                <h1 class="m-0">Tambah Pegawai</h1>
+                <h1 class="m-0">Edit Pegawai</h1>
             </div>
             <!-- /.col -->
             <div class="col-sm-6">
